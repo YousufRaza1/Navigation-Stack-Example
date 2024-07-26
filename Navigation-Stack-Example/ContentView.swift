590d2412f9086e90d9d67c429c5e42be08af1b63
Yousuf Raza <yousuf.raza@brainstation-23.com>
2024-07-27 01:16:42 +0600

    project created

---
+++ Navigation-Stack-Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation-Stack-Example
+//
+//  Created by Yousuf Raza on 27/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation-Stack-Example/Navigation_Stack_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_Stack_ExampleApp.swift
+//  Navigation-Stack-Example
+//
+//  Created by Yousuf Raza on 27/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_Stack_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
