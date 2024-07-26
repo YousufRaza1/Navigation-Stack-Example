8b01ee12c370f383fd50234faf98d0c77bff0727
Yousuf Raza <yousuf.raza@brainstation-23.com>
2024-07-27 01:03:23 +0600

    simple navigation added

---
+++ NavigationStackExample 1.01.23 AM/NavigationStackExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackExampleApp.swift
+//  NavigationStackExample
+//
+//  Created by Yousuf Raza on 25/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationStackExample 1.01.23 AM/SuccessScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SuccessScreen.swift
+//  NavigationStackExample
+//
+//  Created by Yousuf Raza on 25/7/24.
+//
+
+import SwiftUI
+
+struct SuccessScreen: View {
+    var body: some View {
+        Text("Success View")
+    }
+}
+
+#Preview {
+    SuccessScreen()
+}
